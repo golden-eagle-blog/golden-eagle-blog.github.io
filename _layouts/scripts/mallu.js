b20241b32aca67dc5225c36ba0a55f431a02aa1a
golden-eagle-blog <67394336+golden-eagle-blog@users.noreply.github.com>
2020-06-30 23:43:24 +0200

    Create mallu.js

---
+++ _layouts/scripts/mallu.js
@@ -0,0 +1,15 @@
+function mymalluFunction() {
+  var x = document.getElementById("mallu");
+  var y = document.getElementById("parishkari");
+  
+    x.style.display = "block";  
+    y.style.display = "none";  
+}
+
+function myEngFunction() {
+  var x = document.getElementById("mallu");
+  var y = document.getElementById("parishkari");
+  
+    y.style.display = "block";  
+    x.style.display = "none";  
+}
